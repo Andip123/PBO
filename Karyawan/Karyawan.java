96031ba259185dbc173b36237e896ec956e6d8f9
Andi prabandaru <92010838+Andip123@users.noreply.github.com>
2023-04-18 15:17:10 +0700

    Add files via upload

---
+++ Karyawan/Karyawan.java
@@ -0,0 +1,34 @@
+package Karyawan;
+
+/**
+ *
+ * Andi Prabandaru
+ * A11.2021.13907
+ */
+
+public class Karyawan {
+    private String nama;
+    private String nip;
+
+    public Karyawan(String nama, String nip) {
+        this.nama = nama;
+        this.nip = nip;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+    
+}
